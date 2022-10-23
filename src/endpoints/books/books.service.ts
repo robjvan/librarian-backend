ff81b8e97ee3ded045096f4ea9d29f222ca619fd
Rob Vandelinder <rob@eastcoastdev.ca>
2022-10-22 23:15:21 -0230

    Removed dependency

--- src/endpoints/books/books.service.ts
+++ src/endpoints/books/books.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { v4 as uuid } from 'uuid';
+// import { v4 as uuid } from 'uuid';
 import { Book } from 'src/entities/book.entity';
 import { GetBooksFilterDto } from 'src/entities/dto/get-books-with-filters.dto';
 import { BooksRepository } from './books.repository';
