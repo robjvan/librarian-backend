e331e232253c07141c4f1c9d149cf50722cb770a
Rob Vandelinder <rob@eastcoastdev.ca>
2022-10-24 19:46:01 -0230

    Added missing Entity decorator

--- src/entities/book.entity.ts
+++ src/entities/book.entity.ts
@@ -1,6 +1,7 @@
-import { Column, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "./user.entity";
 
+@Entity()
 export class Book {
   @PrimaryGeneratedColumn()
   id: number;
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,12 +1,15 @@
-import { Column, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { IsOptional } from "class-validator";
+import { BeforeInsert, Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Book } from "./book.entity";
 
+
+@Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  @OneToMany(_type => Book, book => book.userID, { eager: true }) /// eager means we will automatically fetch the tasks 
+  // @OneToMany(_type => Book, book => book.userID, { eager: true }) /// eager means we will automatically fetch the tasks 
   id: number;
 
-  @Column()
+  @Column({ unique: true })
   email: string;
   
   @Column()
@@ -15,9 +18,16 @@ export class User {
   @Column()
   password: string;
   
-  @Column()
+  @Column({ default: 'https://placekitten.com/200/200'})
+  @IsOptional()
   profilePicUrl: string;
   
-  @Column()
-  emailConfimed: boolean;
+  @Column('boolean', {default: false})
+  emailConfirmed: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
